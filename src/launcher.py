a5f8632e98f206d7afcb772ac2808488a5ca04b6
iw365 <maxkwillars@gmail.com>
2023-11-22 07:42:02 +0000

    added logo image and split left panel into two parts

--- src/launcher.py
+++ src/launcher.py
@@ -1,8 +1,39 @@
 import tkinter as tk
 import customtkinter as ctk
+from PIL import Image
 import os
 
 from settings import *
+
+class left_sub_frame_top(ctk.CTkFrame):
+    def __init__(self, parent, width, height):
+        self.width = width
+        self.height = height/4
+        super().__init__(parent, width=self.width, height=self.height, fg_color='#0000FF', corner_radius = 0)
+        self.grid_propagate(False)
+        self.initialise_ui()
+        
+    def initialise_ui(self):
+        pass
+        # logo = ctk.CTkImage(light_image=Image.open("assets/logo.png"), dark_image=Image.open("assets/logo.png"), size=(self.width, self.height))
+        # image_label = ctk.CTkLabel(master = self, image=logo, text="")
+        
+class left_sub_frame_bottom(ctk.CTkFrame):
+    def __init__(self, parent, width, height):
+        self.width = width
+        self.height = (height/4)*3
+        super().__init__(parent, width=self.width, height=self.height, fg_color='#FFFF00', corner_radius = 0)
+        self.grid_propagate(False)
+        self.initialise_ui()
+        
+    def initialise_ui(self):
+        app_launch_button = ctk.CTkButton(master=self, text='launch app', command = self.launch_app)
+        app_launch_button.grid(row=0, column=0, padx=0, pady=0)
+        
+    def launch_app(self):
+        root.destroy()
+        
+        os.system('python3 main.py')
 
 class left_frame(ctk.CTkFrame):
     def __init__(self, parent, width, height):
@@ -12,17 +43,16 @@ class left_frame(ctk.CTkFrame):
         self.grid_propagate(False)
         #self.pack_propagate(False)
         
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        # self.grid_rowconfigure(0, weight=1)
+        # self.grid_columnconfigure(0, weight=1)
         self.initialise_ui()
 
     def initialise_ui(self):
-        app_launch_button = ctk.CTkButton(master=self, text='launch app', command = self.launch_app)
-        app_launch_button.grid(row=0, column=0, padx=0, pady=0)
+        self.left_sub_frame_top = left_sub_frame_top(parent = self, width = self.width, height = self.height)
+        self.left_sub_frame_bottom = left_sub_frame_bottom(parent = self, width = self.width, height = self.height)
         
-    def launch_app(self):
-        root.destroy()
-        os.system('python3 main.py')
+        self.left_sub_frame_top.grid(row = 0, column = 0, padx = 0, pady = 0)
+        self.left_sub_frame_bottom.grid(row = 1, column = 0, padx = 0, pady = 0)
 
 class right_frame(ctk.CTkFrame):
     def __init__(self, parent, width, height):
